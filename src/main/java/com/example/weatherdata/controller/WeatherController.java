09521be49ab5c92105d52c3b9346d574c0793660
C GokulLoganathan <C.GokulLoganathan@NorthernArc.com>
2024-03-20 14:39:05 +0530

    added netlify

--- src/main/java/com/example/weatherdata/controller/WeatherController.java
+++ src/main/java/com/example/weatherdata/controller/WeatherController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/weather")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://climacastweaherapp.netlify.app", maxAge = 3600)
 public class WeatherController {
 
     @Autowired
