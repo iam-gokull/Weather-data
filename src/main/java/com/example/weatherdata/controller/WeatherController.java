02318d3b41368f09749906876566adfb635e6c05
C GokulLoganathan <C.GokulLoganathan@NorthernArc.com>
2024-03-20 13:45:31 +0530

    Added cors

--- src/main/java/com/example/weatherdata/controller/WeatherController.java
+++ src/main/java/com/example/weatherdata/controller/WeatherController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/weather")
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WeatherController {
 
     @Autowired
